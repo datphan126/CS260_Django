cadb90eb5a3147ebf50f758b566a1fa8c8492d0b
Dustin <dphant126@gmail.com>
2019-10-13 22:46:41 -0700

    Module2 and Lab2

--- Module2/cs260sandbox/hop/musicstore/payment/urls.py
+++ Module2/cs260sandbox/hop/musicstore/payment/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
 from . import views
 
-from musicstore.payment import views as payment_views
-
 app_name = "payment_ns"
 
 urlpatterns = [
-    url(r'^$', payment_views.pay, name="pay"),
-    url(r'^help/$', payment_views.help, name="help"),
+    url(r'^$', views.pay, name="pay"),
+    url(r'^help/$', views.help, name="help"),
     url(r'^(?P<payment_type>\D+)/',
-        payment_views.pay, {'discount': '40'}),
+        views.pay, {'discount': '40'}, name="pay_detals"),
 ]
+
