61ff91943350771de6a05b5019436acb5e7d0667
Dustin <dphant126@gmail.com>
2019-10-15 23:58:59 -0700

    Lab4

--- Module4/cs260sandbox/hop/musicstore/urls.py
+++ Module4/cs260sandbox/hop/musicstore/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name='homepage.html')),
     url(r'^payment/', include('musicstore.payment.urls',namespace="payment_ns")),
+    url(r'^events/', include('musicstore.events.urls',namespace="events_ns")),
 ]
 
---
+++ Module4_PL/cs260sandbox/lab/BananaStore/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',TemplateView.as_view(template_name='homepage.html')),
+    url(r'^product/', include('BananaStore.product.urls',namespace="product_ns")),
+]
+
